c5988d9076b99119a9b938950bd405e02a2230c8
RobinP122 <robinpljx@gmail.com>
2024-06-14 15:15:29 +0200

    Deserialize string vec example

--- src/api/utils.rs
+++ src/api/utils.rs
@@ -22,7 +22,18 @@ pub async fn retrieve_from_db<D: KvStoreConnection + Clone + Send + 'static>(
     let db_result: Result<Option<Vec<Value>>, _> = db.lock().await.get_data(&address).await;
 
     match db_result {
-        Ok(data) => r.into_ok("Data retrieved successfully", json_serialize_embed(data)),
+        Ok(data) => {
+
+            let elems = data
+                .into_iter()
+                .map(serde_json::from_str)
+                .collect::<Result<Vec<Value>>>()
+                .unwrap();
+
+            let json_val = Value::Array(elems);
+
+            return r.into_ok("Data retrieved successfully", json_serialize_embed(data))
+        },
         Err(_) => r.into_err_internal(ApiErrorType::Generic(
             "Full Valence chain retrieval failed".to_string(),
         )),
